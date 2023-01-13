01431901ee6112eaf1b9fa6e2ac43c21a5bc8754
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2023-01-13 18:07:33 -0400

    build(main) Configured Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'api_constraints'
 
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
-        end
+    end
   end
 end
